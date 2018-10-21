f72e8beaf3874f27375b26779b188dc004df3abd
Ronan Browne <ronanbrowne88@gmail.com>
2018-10-21 11:00:50 +0100

    initial commit
    project set up ,....

---
+++ src/main/java/com/browner/lambda/demo/HelloInput.java
@@ -0,0 +1,18 @@
+package com.browner.lambda.demo;
+
+//lamdba needs getter and setter
+public class HelloInput {
+
+	
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
---
+++ src/main/java/com/browner/lambda/demo/HelloOutput.java
@@ -0,0 +1,30 @@
+package com.browner.lambda.demo;
+
+public class HelloOutput {
+	
+	String message;
+	String functionName;
+	int memoryLimit;
+	
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getFunctionName() {
+		return functionName;
+	}
+	public void setFunctionName(String functionName) {
+		this.functionName = functionName;
+	}
+	public int getMemoryLimit() {
+		return memoryLimit;
+	}
+	public void setMemoryLimit(int memoryLimit) {
+		this.memoryLimit = memoryLimit;
+	}
+	
+	
+
+}
